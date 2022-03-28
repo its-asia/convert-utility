b859c8521883a6b52602c4df02356d84147966d6
charlie <96938527+its-asia@users.noreply.github.com>
2022-03-28 15:23:22 -0700

    Update fakeremote.lua

--- documentation/fakeremote.lua
+++ documentation/fakeremote.lua
@@ -1,1 +1,17 @@
+local FakeRemoteLink = 'https://github.com/its-asia/convert-utility/blob/main/fakeremote.lua'
+local FakeRemote = loadstring(game:HttpGet(FakeRemoteLink, true))()
 
+-- add the script above, at the beginning of your convert
+-- old script below
+
+local Remote = Instance.new('RemoteEvent')
+
+-- new script below
+
+local Remote = FakeRemote.new('RemoteEvent')
+
+-- below code will now work
+-- (fire client and onclientevent dont work!)
+
+Remove.OnServerEvent:Connect(function(Player, Print) print(Print) end) -- Player will ALWAYS be the localplayer
+Remote:FireServer('Hello, world!')
