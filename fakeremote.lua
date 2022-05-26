f13035fc04ac7f17b0c38233188473dbfb3197b5
charlie <96938527+its-asia@users.noreply.github.com>
2022-05-25 21:29:25 -0700

    Update fakeremote.lua

--- fakeremote.lua
+++ fakeremote.lua
@@ -4,53 +4,70 @@ local LocalPlayer = Players.LocalPlayer
 return {
 	new = function(Type)
 		local BindableOnEvent = Instance.new('BindableEvent')
-		
+
 		local EmptyFunction = function() end
 		local FakeRemote = {OnServerEvent = BindableOnEvent.Event, FireServer = EmptyFunction, OnServerInvoke = EmptyFunction, InvokeServer = EmptyFunction} -- set all possible functions for syntax purposes
-		
+
 		local OnEvent
-		local FireEvent
+		local ClientOnEvent
 		
+		local FireEvent
+		local ClientFireEvent
+		
+		local FireAll
+
 		local Bindable
 		if Type == 'RemoteEvent' then
 			OnEvent = 'OnServerEvent'
-			FireEvent = 'FireServer'
+			ClientOnEvent = 'OnClientEvent'
 			
+			FireEvent = 'FireServer'
+			ClientFireEvent = 'FireClient'
+			
+			FireAll = 'FireAllClients'
+
 			Bindable = false
 		elseif Type == 'RemoteFunction' then
 			OnEvent = 'OnServerInvoke'
-			FireEvent = 'InvokeServer'
+			ClientOnEvent = 'OnClientInvoke'
 			
+			FireEvent = 'InvokeServer'
+			ClientFireEvent = 'InvokeClient'
+
 			Bindable = true
 		end
-		
+
 		if OnEvent == nil and FireEvent == nil and Bindable == nil then
 			error('fakeremote.new requires a valid remote type!')
 		end
-		
+
 		FakeRemote[FireEvent] = function(Self, ...) -- :FireServer equivalent
 			local Function = Self[OnEvent]
 			if not Function then error('No function set.') end
-			
+
 			if typeof(Function) == 'RBXScriptSignal' then
 				local Return = BindableOnEvent:Fire(LocalPlayer, ...)
 				return Return
 			end
-			
+
 			return Function(LocalPlayer, ...)
 		end
-		
+
+		FakeRemote[ClientFireEvent] = FakeRemote[FireEvent]
+
 		if Bindable == true then -- Remote functions will have their own function set
 			BindableOnEvent:Destroy()
-			
+
 			return FakeRemote
 		end
+
+		FakeRemote[OnEvent] = BindableOnEvent.Event
+		FakeRemote[ClientOnEvent] = FakeRemote[OnEvent]
 		
-		FakeRemote[OnEvent] = BindableOnEvent.Event					
 		FakeRemote['Destroy'] = function(Self)
 			if Self == FakeRemote then
 				BindableOnEvent:Destroy()
-				
+
 				for Index, _ in pairs(FakeRemote) do
 					FakeRemote[Index] = nil
 				end
@@ -58,7 +75,7 @@ return {
 				Self:Destroy()
 			end
 		end
-
+		
 		return FakeRemote
 	end,
 }
