c53f6e5afd19a14a7452f9289275458974ebf740
charlie <96938527+its-asia@users.noreply.github.com>
2022-06-28 19:33:52 -0700

    Create wrappedremote.lua

---
+++ documentation/wrappedremote.lua
@@ -0,0 +1,32 @@
+-- any time .Parent is changed
+-- you MUST do .Instance.Parent
+
+-- example
+-- local Remote = (github stuff)
+-- Remote.Instance.Parent = workspace
+
+local Services = {}
+local MetaServices = {__index = function(self, Index) return rawget(Services, Index) or game:GetService(Index) end}
+
+setmetatable(Services, MetaServices)
+
+return function(Remote)
+	local Override = {}
+	
+	local UserData = newproxy(true)
+	local MetaTable = getmetatable(UserData)
+	
+	Override.Instance = Remote
+	MetaTable.__index = function(self, Index)
+		return Override[Index] or Remote[Index]
+	end
+	
+	MetaTable.__newindex = function(self, Index, Value)
+		Remote[Index] = Value
+	end
+	
+	MetaTable.__metatable = 'The metatable is locked'
+	MetaTable.__tostring = function() return tostring(Remote) end
+	
+	return UserData
+end
